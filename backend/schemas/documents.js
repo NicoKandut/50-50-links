0e4c07e8158e1170926c070b5fcd5aac4b24471c
Christian Winkler <chris@bashit.me>
2018-09-24 11:15:27 +0200

    Added pre save protocol check

--- backend/schemas/documents.js
+++ backend/schemas/documents.js
@@ -15,6 +15,23 @@ const documentSchema = new Schema(
     }
   }
 );
+/*documentSchema.pre('save', function(next){
+  var newDoc = this;
+  for(var i = 0; i < newDoc.urls.lenght; i++){
+    if(newDoc.urls[i].substring(0,5) == "http:" || newDoc.urls[i].substring(0,6) == "https:") {
+      switch(newDoc.urls[i].split(":")[0]){
+        case "http":  if(!/.\..+\./g.test(newDoc.urls[i].split("//")[1] )) newDoc.urls[i] = "http://www." + newDoc.urls[i].split("://")[1];
+                      break;
+        case "https": if(!/.\..+\./g.test(newDoc.urls[i].split("//")[1] )) newDoc.urls[i] = "https://www." + newDoc.urls[i].split("://")[1];
+                      break;
+      }
+    } else {
+      if(!/.\..+\./g.test(newDoc.urls[i])) newDoc.urls[i] = "www." + newDoc.urls[i];
+      newDoc.urls[i] = "http://" + newDoc.urls[i];
+    }
+  }
+  next();
+});*/
 
 function arrayMin(val) {
   return val.length > 1;
